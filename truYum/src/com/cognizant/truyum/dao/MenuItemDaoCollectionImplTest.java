f409c64876d8cb62c42ae067bca269a0c947d9cb
Blitzar9 <80390976+Blitzar9@users.noreply.github.com>
2021-03-18 20:35:47 +0530

    Update MenuItemDaoCollectionImplTest.java

--- truYum/src/com/cognizant/truyum/dao/MenuItemDaoCollectionImplTest.java
+++ truYum/src/com/cognizant/truyum/dao/MenuItemDaoCollectionImplTest.java
@@ -1,5 +1,20 @@
 package com.cognizant.truyum.dao;
 
 public class MenuItemDaoCollectionImplTest {
+	public static void main(String args[]) {
+		
+	}
+	public static void testGetMenuItemListAdmin() {
+		
+	}
+	public static void testGetMenuItemListCustomer() {
+		
+	}
+	public static void testModifyMenuItem() {
+		
+	}
+	public static void testGetMenuItem() {
+		
+	}
 
 }
