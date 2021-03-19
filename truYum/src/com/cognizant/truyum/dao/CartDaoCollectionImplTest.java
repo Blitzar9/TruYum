cbb2ab30d49de731f68f31e0bb09d42444b3e2de
Blitzar9 <80390976+Blitzar9@users.noreply.github.com>
2021-03-19 14:50:38 +0530

    Update CartDaoCollectionImplTest.java

--- truYum/src/com/cognizant/truyum/dao/CartDaoCollectionImplTest.java
+++ truYum/src/com/cognizant/truyum/dao/CartDaoCollectionImplTest.java
@@ -1,17 +1,53 @@
 package com.cognizant.truyum.dao;
 
+import java.util.List;
+
+import com.cognizant.truyum.model.MenuItem;
+
 public class CartDaoCollectionImplTest {
-	public static void main(String args[]) {
-		
-	}
-	public static void testAddCardItem() {
-		
-	}
-	public static void testGetAllCartItems() {
-		
-	}
-	public static void testRemoveCartItem() {
-		
+
+	public static void main(String[] args) throws CartEmptyException {
+		testAddCartItem();
+		testGetAllCartItems();
+		testremoveCartItem();
 	}
 
+	static void testAddCartItem() throws CartEmptyException {
+		CartDaoCollectionImpl cartDaoCollectionImpl = new CartDaoCollectionImpl();
+		CartDao cartDao = cartDaoCollectionImpl;
+		cartDao.addCartItem(1, 000004);
+		cartDao.addCartItem(1, 000002);
+		List<MenuItem> menuItemList = cartDao.getAllCartItems(1);
+		// System.out.println("Cart List after adding : " + menuItemList);
+		System.out.println("Cart List after adding : ");
+		for (MenuItem menuItem : menuItemList) {
+			System.out.println(menuItem.toString());
+		}
+		System.out.println();
+
+	}
+
+	static void testGetAllCartItems() throws CartEmptyException {
+		CartDaoCollectionImpl cartDaoCollectionImpl = new CartDaoCollectionImpl();
+		CartDao cartDao = cartDaoCollectionImpl;
+		List<MenuItem> menuItemList = cartDao.getAllCartItems(1);
+		System.out.println("Cart List : ");
+		for (MenuItem menuItem : menuItemList) {
+			System.out.println(menuItem.toString());
+		}
+		System.out.println();
+
+	}
+
+	static void testremoveCartItem() throws CartEmptyException {
+		CartDaoCollectionImpl cartDaoCollectionImpl = new CartDaoCollectionImpl();
+		CartDao cartDao = cartDaoCollectionImpl;
+		cartDao.removeCartItem(1, 00004);
+		List<MenuItem> menuItemList = cartDao.getAllCartItems(1);
+		System.out.println("Cart List after removal of MenuItem with id=4:");
+		for (MenuItem menuItem : menuItemList) {
+			System.out.println(menuItem.toString());
+		}
+		System.out.println();
+	}
 }
